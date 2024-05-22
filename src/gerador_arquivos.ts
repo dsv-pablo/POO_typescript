0a71560327d6a33624d1cfc5a12fd9df3487eb90
Pablo Vieira <63684077+dsv-pablo@users.noreply.github.com>
2024-05-22 16:20:18 +0000

    Cria e executa script de geracao de arquivos de contatos

---
+++ src/gerador_arquivos.ts
@@ -0,0 +1,39 @@
+import { faker } from "@faker-js/faker";
+import { writeFileSync } from "fs";
+import { join } from "path";
+
+interface IUser {
+  nome: string;
+  telefone: string;
+  email: string;
+  dataNascimento: Date;
+  sexo: string;
+}
+
+const contatos: IUser[] = [];
+let linhaCSV = "";
+const numeroContatos = 100;
+
+for (let i = 0; i < numeroContatos; i++) {
+  const sexo = faker.person.sexType();
+  const obj = {
+    sexo: sexo,
+    nome: faker.person.firstName(sexo),
+    telefone: faker.phone.number(),
+    email: faker.internet.email(),
+    dataNascimento: faker.date.birthdate(),
+  };
+  contatos.push(obj);
+  const linha = `${obj.sexo},${obj.nome}, ${obj.telefone}, ${obj.email}, ${obj.dataNascimento}`;
+  linhaCSV += linha + `\n`;
+}
+
+// Gravando arquivo JSON
+const caminhoDiretorioArquivos = join(__dirname, "data"); // __driname é uma variável global que retorna o caminho atual
+const strJSON = JSON.stringify(contatos); // Converte o array contatos em uma string JSON
+const caminhoArquivoJSON = join(caminhoDiretorioArquivos, "contatos.json");
+writeFileSync(caminhoArquivoJSON, strJSON);
+
+// Gravando arquivo CSV
+const caminhoArquivoCSV = join(caminhoDiretorioArquivos, "contatos.csv");
+writeFileSync(caminhoArquivoCSV, linhaCSV);
