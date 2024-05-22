84bcf02c5d4b8907dc618acd8e1531b3300e3243
Pablo Vieira <63684077+dsv-pablo@users.noreply.github.com>
2024-05-22 00:24:12 +0000

    Adiciona os metodos sacar e depositar

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,22 @@ const conta1 = new ContaBancaria();
 
 const saldo1 = conta1.consultarSaldo();
 
-console.log(`O saldo da conta é: ${saldo1}`);
+console.log(`Saldo inicial : ${saldo1}`);
+
+const deposito1 = conta1.depositar(10000);
+
+if (deposito1) {
+  const saldo2 = conta1.consultarSaldo();
+  console.log(`Saldo atual: ${saldo2}`);
+} else {
+  console.log("[ERRO] Tentativa de deposito de valor invalido!");
+}
+
+const saque1 = conta1.sacar(7300);
+
+if (saque1) {
+  const saldo3 = conta1.consultarSaldo();
+  console.log(`Saldo atual: ${saldo3}`);
+} else {
+  console.log("[ERRO] Tentativa de saque de valor invalido!");
+}
--- src/models/ContaBancaria.ts
+++ src/models/ContaBancaria.ts
@@ -4,6 +4,9 @@
     
     - Se o saldo pudesse ser iniciado com algum valor, por exemplo,
         ele seria recebido como parâmetro do constructor
+    
+    - Quando não indicamos diretamente o tipo de dado do retorno
+        de uma função, o TS infere automaticamente
 
 */
 
@@ -16,5 +19,21 @@ export class ContaBancaria {
 
   consultarSaldo() {
     return this.saldo;
+  }
+
+  sacar(valor: number): boolean {
+    if (valor > 0 && this.saldo >= valor) {
+      this.saldo -= valor;
+      return true;
+    }
+    return false;
+  }
+
+  depositar(valor: number): boolean {
+    if (valor > 0) {
+      this.saldo += valor;
+      return true;
+    }
+    return false;
   }
 }
