2d731bc8959876294b3f1cef9daae0e5a8b9cbcd
Pablo Vieira <63684077+dsv-pablo@users.noreply.github.com>
2024-05-22 15:01:10 +0000

    Inicializa ts-jest e cria bateria de testes para consultar saldo, sacar e depositar valores

--- src/models/ContaBancaria.test.ts
+++ src/models/ContaBancaria.test.ts
@@ -1,3 +1,29 @@
-test("1 + 1 deve ser 2", () => {
-  expect(1 + 1).toBe(2);
+import { ContaBancaria } from "./ContaBancaria";
+
+describe("Testa as funcionalidades de uma Conta Bancária", () => {
+  test("Deve criar uma conta com agência e conta corretamente passados como parâmetros", () => {
+    const conta = new ContaBancaria("01234-1", "0123-1");
+    expect(conta.conta).toBe("01234-1");
+    expect(conta.agencia).toBe("0123-1");
+  });
+
+  test("O saldo de uma conta recém criada deve ser nulo", () => {
+    const conta = new ContaBancaria("01234-1", "0123-1");
+    expect(conta.consultarSaldo()).toBe(0);
+  });
+
+  test("O saldo deve ser alterado corretamente após realizar depósitos", () => {
+    const conta = new ContaBancaria("01234-1", "0123-1");
+    conta.depositar(23.75);
+    conta.depositar(38);
+    expect(conta.consultarSaldo()).toBe(61.75);
+  });
+
+  test("O saldo deve ser alterado corretamente após realizar saques", () => {
+    const conta = new ContaBancaria("01234-1", "0123-1");
+    conta.depositar(10350.32);
+    conta.sacar(350);
+    conta.sacar(100.27);
+    expect(conta.consultarSaldo()).toBe(9900.05);
+  });
 });
